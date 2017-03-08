c3a0eb57dfe9c5476d8344841dabdaed58fa9d9c
connerturner <conner.turner@exnntech.co.uk>
2017-03-08 23:45:26 +0000

    Edit readme to add usage

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This is an alerting system to send push-notifications in realtime to subscribed clients, originally developed to send notifications when certain busses arrive at Runshaw College
 
+#####Usage
+Go to ![](http://bus.connerturner.net) and subscribe to notifications
+
 ____
 ####Releases:
 Version | Changelog |
@@ -9,6 +12,6 @@ Version | Changelog |
 
 
 ____
-####Installing WSK
-This progressive web application is built ontop of the Web Start Kit standard, therefore requires node and gulp to run testing and serve content, to install run:
+####Contributing
+Fok this repo, its a progressive web application built ontop of the Web Start Kit standard, therefore requires node and gulp to run testing and serve content, to install run:
 `npm install --global gulp && npm install`
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,8 @@
-/*!
- *
- *  Web Starter Kit
- *  Copyright 2015 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
 /* eslint-env browser */
 (function() {
   'use strict';
 
-  // Check to make sure service workers are supported in the current browser,
-  // and that the current page is accessed from a secure origin. Using a
-  // service worker from an insecure origin will trigger JS console errors. See
-  // http://www.chromium.org/Home/chromium-security/prefer-secure-origins-for-powerful-new-features
+  // Check to make sure service workers are supported in the current browser
   var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
       // [::1] is the IPv6 localhost address.
       window.location.hostname === '[::1]' ||
@@ -37,15 +16,8 @@
       (window.location.protocol === 'https:' || isLocalhost)) {
     navigator.serviceWorker.register('service-worker.js')
     .then(function(registration) {
-      // updatefound is fired if service-worker.js changes.
       registration.onupdatefound = function() {
-        // updatefound is also fired the very first time the SW is installed,
-        // and there's no need to prompt for a reload at that point.
-        // So check here to see if the page is already controlled,
-        // i.e. whether there's an existing service worker.
         if (navigator.serviceWorker.controller) {
-          // The updatefound event implies that registration.installing is set:
-          // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
           var installingWorker = registration.installing;
 
           installingWorker.onstatechange = function() {
@@ -72,9 +44,5 @@
     });
   }
 
-// Loading Feedback
-  document.onload = function() {
-    var progressBar = document.getElementById('progressBar');
-    progressBar.style.display = 'hidden';
-  };
+  
 })();
