610541702bb2f21363a051a55db7b141350aeb7c
connerturner <conner.turner@exnntech.co.uk>
2017-03-19 21:35:34 +0000

    adding push service worker registration

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,47 +1,27 @@
 /* eslint-env browser */
-/*eslint linebreak-style: [2, "windows"]*/
+/* eslint linebreak-style: [2, "windows"]*/
+/* eslint no-unused-vars: 0*/
 (function() {
   'use strict';
 
-  // Check to make sure service workers are supported in the current browser
-  var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
-      // [::1] is the IPv6 localhost address.
-      window.location.hostname === '[::1]' ||
-      // 127.0.0.1/8 is considered localhost for IPv4.
-      window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-      )
-    );
+  var isSubscribed = false;
+  var swRegistration;
+  var pushButton = document.querySelector('#btnSubscribe');
 
-  if ('serviceWorker' in navigator &&
-      (window.location.protocol === 'https:' || isLocalhost) && 'PushManager' in window) {
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    console.log('Service Worker and Push is supported');
+
     navigator.serviceWorker.register('service-worker.js')
-    .then(function(registration) {
-      registration.onupdatefound = function() {
-        if (navigator.serviceWorker.controller) {
-          var installingWorker = registration.installing;
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
 
-          installingWorker.onstatechange = function() {
-            switch (installingWorker.state) {
-              case 'installed':
-                // At this point, the old content will have been purged and the
-                // fresh content will have been added to the cache.
-                // It's the perfect time to display a "New content is
-                // available; please refresh." message in the page's interface.
-                break;
-
-              case 'redundant':
-                throw new Error('The installing ' +
-                                'service worker became redundant.');
-
-              default:
-                // Ignore
-            }
-          };
-        }
-      };
-    }).catch(function(e) {
-      console.error('Error during service worker registration:', e);
-    });
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+  } else {
+    console.warn('Push messaging is not supported');
+    pushButton.textContent = 'Push Not Supported';
   }
 })();
