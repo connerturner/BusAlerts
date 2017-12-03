fd68c86e7a4fc1d2310da1cdedf209d6a3b889f3
Conner Turner <cturner@zyme.xyz>
2017-12-03 21:41:27 +0000

    tracing master rbanch

--- alerts/src/routes/home/index.js
+++ alerts/src/routes/home/index.js
@@ -1,5 +1,6 @@
 import { h, Component } from 'preact';
 import Card from 'preact-material-components/Card';
+import Button from 'preact-material-components/Button';
 import 'preact-material-components/Card/style.css';
 import 'preact-material-components/Button/style.css';
 import style from './style';
@@ -8,19 +9,7 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div class={style.home}>
-				<h1>Home route</h1>
-				<Card>
-					<Card.Primary>
-						<Card.Title>Home card</Card.Title>
-						<Card.Subtitle>Welcome to home route</Card.Subtitle>
-					</Card.Primary>
-					<Card.SupportingText>
-						Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-					</Card.SupportingText>
-					<Card.Actions>
-						<Card.Action>OKAY</Card.Action>
-					</Card.Actions>
-				</Card>
+				<h1>Manage Notifications</h1>
 			</div>
 		);
 	}
