18c4896b501b7b20dd73c5ad652c41e3cb0226b9
Conner Turner <cturner@zyme.xyz>
2017-12-03 21:42:24 +0000

    Editing default styles

--- alerts/src/routes/home/index.js
+++ alerts/src/routes/home/index.js
@@ -3,6 +3,10 @@ import Card from 'preact-material-components/Card';
 import Button from 'preact-material-components/Button';
 import 'preact-material-components/Card/style.css';
 import 'preact-material-components/Button/style.css';
+import Select from 'preact-material-components/Select';
+import 'preact-material-components/List/style.css';
+import 'preact-material-components/Menu/style.css';
+import 'preact-material-components/Select/style.css';
 import style from './style';
 
 export default class Home extends Component {
@@ -10,6 +14,12 @@ export default class Home extends Component {
 		return (
 			<div class={style.home}>
 				<h1>Manage Notifications</h1>
+				<Select hintText="Select a Bus Number" class={style.select}>
+            		<Select.Item>opt1</Select.Item>
+            		<Select.Item>opt2</Select.Item>
+            		<Select.Item>opt3</Select.Item>
+            		<Select.Item>opt4</Select.Item>
+        		</Select>
 			</div>
 		);
 	}
