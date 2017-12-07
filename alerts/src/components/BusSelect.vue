d181be47f4af68f0208dbbc8a64f50c2757eeac9
Conner Turner <cturner@zyme.xyz>
2017-12-07 23:31:20 +0000

    Created Bus Selection component

---
+++ alerts/src/components/BusSelect.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="busselection">
+    <h3>{{sectionTitle}}</h3>
+    <select>
+        <option v-for="bus in busList">{{bus.number}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'busselect',
+  data () {
+    return {
+      busList: [
+        {number: '762'},
+        {number: '763'},
+        {number: '764'},
+        {number: '765'},
+        {number: '766'}
+      ],
+      sectionTitle: 'Select your bus number:'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
